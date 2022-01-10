cef488ebcd9e393a19da23813202969f8cc8a87e
tfrauenfeld <tfrauenfeld@email.arizona.edu>
2022-01-10 15:30:12 -0700

    Uploading code for Valid Braces Kata.

---
+++ Python/ValidBraces.py
@@ -0,0 +1,31 @@
+"""
+Write a function that takes a string of braces, and determines if the order of the braces is valid. 
+It should return true if the string is valid, and false if it's invalid.
+
+All input strings will be nonempty, and will only consist of parentheses, brackets and curly 
+braces: ()[]{}.
+
+What is considered Valid?
+
+A string of braces is considered valid if all braces are matched with the correct brace.
+
+Examples:
+
+"(){}[]"   =>  True
+"([{}])"   =>  True
+"(}"       =>  False
+"[(])"     =>  False
+"[({})](]" =>  False
+"""
+def validBraces(string):
+    pairs = {'(':')', '{':'}', '[':']'}
+    braces = list()
+
+    for character in string:
+        if character in pairs.keys():
+            braces.append(character)
+        else:
+            if len(braces) == 0 or character != pairs[braces.pop()]:
+                return False
+
+    return len(braces) == 0
